72419dd6cae1a6e3d81d260f5776009dfe72a4af
Ramola Shishupal <46933986+RamolaShishupal@users.noreply.github.com>
2020-06-09 16:50:31 +0530

    Add files via upload
    java files added

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.rainometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rainometer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
